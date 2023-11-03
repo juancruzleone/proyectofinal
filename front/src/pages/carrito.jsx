221439f06ce20c467e0730a8f995ada0a0b0dd8e
Juan cruz Leone <juancruzleone@outlook.com>
2023-11-03 16:30:38 -0300

    Se creo carrito y detalle de productos

---
+++ front/src/pages/carrito.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/layout";
+import Footer from "@/components/Footer";
+import styles from "@/styles/Home.module.css";
+
+const carrito = () => {
+  return (
+    <Layout className={styles.app}>
+      <h1 className={styles.tituloPaginas}>Carrito</h1>
+      <div className={styles.contenedorSeccionCarrito}>
+        <div className={styles.contenedorCarrito}>
+          <h3>Productos en el carrito</h3>
+        
+        </div>
+        <div className={styles.contenedorDatosPagos}>
+
+        </div>
+      </div>
+      
+      <Footer></Footer>
+    </Layout>
+  );
+};
+
+export default carrito;
